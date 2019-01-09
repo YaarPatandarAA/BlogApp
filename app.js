651e990ce9b59e5ee4c17b896e7174b26dd55818
Amarjit Singh <b.singh.amarjit@gmail.com>
2019-01-09 14:18:49 -0800

    add schema

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ express        = require("express"),
 app            = express();
 
 // APP CONFIG
-mongoose.connect("mongodb://localhost/restful_blog_app");
+mongoose.connect('mongodb://localhost:27017/restful_blog_app', { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+// MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
 
 app.listen(3000, function(){
     console.log("SERVER IS RUNNING!");
