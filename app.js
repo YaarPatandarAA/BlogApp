7a226f3b008b8e2dc92b749c36a458856de5f419
Amarjit Singh <b.singh.amarjit@gmail.com>
2019-01-09 14:23:58 -0800

    blog route

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
 var bodyParser = require("body-parser"),
-mongoose       = require("mongoose"),
-express        = require("express"),
-app            = express();
+    mongoose = require("mongoose"),
+    express = require("express"),
+    app = express();
 
 // APP CONFIG
 mongoose.connect('mongodb://localhost:27017/restful_blog_app', { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
-    created: {type: Date, default: Date.now}
+    created: { type: Date, default: Date.now }
 });
 var Blog = mongoose.model("Blog", blogSchema);
 
-app.listen(3000, function(){
+// RESTFUL ROUTES
+
+app.get("/", function (req, res) {
+    res.redirect("/blogs");
+});
+
+// INDEX ROUTE
+app.get("/blogs", function (req, res) {
+    Blog.find({}, function (err, blogs) {
+        if (err) {
+            console.log("ERROR!");
+        } else {
+            res.render("index", { blogs: blogs });
+        }
+    });
+});
+
+
+app.listen(3000, function () {
     console.log("SERVER IS RUNNING!");
 })
