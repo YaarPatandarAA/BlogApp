e70ec297fc942192c60271290d1656e0b37c36f7
Amarjit Singh <b.singh.amarjit@gmail.com>
2019-01-09 14:50:29 -0800

    Update app.js

--- app.js
+++ app.js
@@ -43,9 +43,6 @@ app.get("/blogs/new", function (req, res) {
 // CREATE ROUTE
 app.post("/blogs", function (req, res) {
     // create blog
-    console.log(req.body);
-    console.log("===========")
-    console.log(req.body);
     Blog.create(req.body.blog, function (err, newBlog) {
         if (err) {
             res.render("new");
