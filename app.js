1749ec7d6a2aeac5c59ee39d536a75d53327d554
Amarjit Singh <b.singh.amarjit@gmail.com>
2019-01-09 14:58:14 -0800

    edit and update routing

--- app.js
+++ app.js
@@ -64,6 +64,30 @@ app.get("/blogs/:id", function(req, res){
     })
  });
 
+ // EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+})
+
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body)
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+      if(err){
+          res.redirect("/blogs");
+      }  else {
+          res.redirect("/blogs/" + req.params.id);
+      }
+   });
+});
+
 app.listen(3000, function () {
     console.log("SERVER IS RUNNING!");
 })
